0ef6addda29b9aa68f7e0717e0a24b2d758b1667
Velmor99 <wincentwangog@gmail.com>
2024-08-02 04:34:23 +0300

    added routes

---
+++ routes.ts
@@ -0,0 +1,29 @@
+export const routes = {
+  main: "/",
+  resume: "/resume",
+  speakingClub: "/speaking-club",
+  documents: "/documents",
+  students: "/students",
+  prepareToTest: "/prepare-to-test",
+  consultation: "/consultation",
+  localization: {
+    en: {route: "en", title: "english"},
+    de: {route: "de", title: "deutsch"},
+    // ua: {route: "ua", title: "українська"},
+    ru: {route: "ru", title: "русский"}
+  },
+  publicOffer: "/public-offer",
+  privacyPolicy: "/privacy-policy",
+  freeCourse: "/",
+  languageSchool: "/"
+}
+
+export const links = {
+  telegram: "",
+  whatsapp: "",
+  instagram: "",
+  youtube: "",
+  email: "mailto:weinfo@welingua.com",
+  phone: "tel:+49 6221 825 96 40",
+
+}

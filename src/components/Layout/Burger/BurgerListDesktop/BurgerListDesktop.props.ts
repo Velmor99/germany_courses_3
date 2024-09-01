47bc3c9207e97d0758c69033b64f04d427bb24fe
roman <wincentwangog@gmail.com>
2024-09-01 23:54:08 +0300

    remove layout

--- src/components/Layout/Burger/BurgerListDesktop/BurgerListDesktop.props.ts
+++
@@ -1,5 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes } from "react";
-
-export interface BurgerMenuDesktopProps extends DetailedHTMLProps<HTMLAttributes<HTMLUListElement>, HTMLUListElement> {
-  isOpened: boolean;
-}
--- src/components/Layout/Burger/BurgerMenuButton/BurgerMenuButton.props.ts
+++
@@ -1,6 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes } from 'react';
-
-export interface BurgerButtonMenuProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-  changeMenu: () => void;
-  isShowed: boolean;
-}
